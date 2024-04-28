2bd289a4b85b5a73485d817d1706746ee48f6235
Sander Verbrugge <32176563+sandcore-dev@users.noreply.github.com>
2024-04-28 22:08:34 +0200

    Moved trait to Concerns namespace

--- app/Concerns/FormattedDuration.php
+++ app/Concerns/FormattedDuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Traits;
+namespace App\Concerns;
 
 use Carbon\CarbonInterface;
 use Carbon\CarbonInterval;
--- app/Models/Game.php
+++ app/Models/Game.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Traits\FormattedDuration;
+use App\Concerns\FormattedDuration;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
