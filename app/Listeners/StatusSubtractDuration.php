8e073fe0c2246ba1b6d9bcd6b7a4f84acd1d02c0
Sander Verbrugge <32176563+sandcore-dev@users.noreply.github.com>
2024-04-28 22:16:02 +0200

    Moved code from listener to support class

--- app/Listeners/StatusSubtractDuration.php
+++ app/Listeners/StatusSubtractDuration.php
@@ -3,26 +3,12 @@
 namespace App\Listeners;
 
 use App\Events\ActivityDeleted;
+use App\Support\Duration;
 
 class StatusSubtractDuration
 {
-    /**
-     * Handle the event.
-     *
-     * @param ActivityDeleted $event
-     * @return void
-     */
     public function handle(ActivityDeleted $event): void
     {
-        $activity = $event->activity;
-        if ($activity->stopped_at === null) {
-            return;
-        }
-
-        $duration = $activity->stopped_at->diffInSeconds($activity->started_at);
-
-        $activity->status->decrement('duration', $duration);
-        $activity->status->level->decrement('duration', $duration);
-        $activity->status->level->game->decrement('duration', $duration);
+        Duration::subtract($event->activity);
     }
 }
---
+++ app/Support/Duration.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Support;
+
+use App\Models\Activity;
+use Illuminate\Support\Carbon;
+
+class Duration
+{
+    public static function update(Activity $activity): void
+    {
+        if ($activity->isClean(['started_at', 'stopped_at']) || $activity->stopped_at === null) {
+            return;
+        }
+
+        $duration = $activity->stopped_at->diffInSeconds($activity->started_at, true);
+
+        $orgStartedAt = $activity->getOriginal('started_at');
+        $orgStoppedAt = $activity->getOriginal('stopped_at');
+
+        if ($orgStoppedAt instanceof Carbon) {
+            $duration -= $orgStoppedAt->diffInSeconds($orgStartedAt);
+        }
+
+        $activity->status->increment('duration', $duration);
+        $activity->status->level->increment('duration', $duration);
+        $activity->status->level->game->increment('duration', $duration);
+    }
+
+    public static function subtract(Activity $activity): void
+    {
+        if ($activity->stopped_at === null) {
+            return;
+        }
+
+        $duration = $activity->stopped_at->diffInSeconds($activity->started_at);
+
+        $activity->status->decrement('duration', $duration);
+        $activity->status->level->decrement('duration', $duration);
+        $activity->status->level->game->decrement('duration', $duration);
+    }
+}
