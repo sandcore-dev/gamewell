8557b74c848849d251468f17f0bae7bfe2da97b9
Sander Verbrugge <32176563+sandcore-dev@users.noreply.github.com>
2025-05-16 21:10:23 +0200

    Added option to recalculate durations for all games

--- app/Console/Commands/RecalculateDurations.php
+++ app/Console/Commands/RecalculateDurations.php
@@ -9,14 +9,26 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class RecalculateDurations extends Command
 {
-    protected $signature = 'recalculate-durations {id-or-slug : ID or slug from game}';
+    protected $signature = 'recalculate-durations
+        {id-or-slug? : ID or slug from game}
+        {--all : Recalculate durations for all games}
+    ';
 
     protected $description = 'Recalculate durations for a game';
 
     public function handle(): int
     {
+        if ($this->option('all')) {
+            Game::eachById([Duration::class, 'recalculate']);
+            return self::SUCCESS;
+        }
+
+        if (!$this->argument('id-or-slug')) {
+            $this->error('No ID or slug provided');
+            return self::INVALID;
+        }
+
         try {
-            /** @var Game $game */
             $game = Game::query()
                 ->where('id', $this->argument('id-or-slug'))
                 ->orWhere('slug', $this->argument('id-or-slug'))
