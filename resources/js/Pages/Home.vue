a28ac7534fdffad560928277938d423d87de892c
Sander Verbrugge <32176563+sandcore-dev@users.noreply.github.com>
2025-05-16 20:13:34 +0200

    Added route to link to game

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -2,6 +2,7 @@
 import Layout from '@/Layouts/Layout.vue';
 import TitleBar from '@/Components/TitleBar.vue';
 import Anchor from '@/Components/Anchor.vue';
+import { route } from 'ziggy-js';
 
 defineProps({
     activitiesByDay: {
@@ -39,18 +40,25 @@ defineProps({
         <title-bar class="mb-3">
             {{ day }}
         </title-bar>
-        <div v-for="(activity) in activities" :key="activity.id" class="flex flex-column">
+        <div
+            v-for="({id, status, formatted_duration}) in activities"
+            :key="id"
+            class="flex flex-column"
+        >
             <div class="flex-grow">
-                <anchor :href="activity.status.level.game.slug" class="hover:text-cyan-400">
-                    {{ activity.status.level.game.name }}
+                <anchor
+                    :href="route('game.show', status.level.game.slug)"
+                    class="hover:text-cyan-400"
+                >
+                    {{ status.level.game.name }}
                 </anchor>
                 <span class="pl-3 font-bold text-cyan-400">
-                {{ activity.status.level.name }}
-                    - {{ $t('attempt :attempt', activity.status) }}
+                {{ status.level.name }}
+                    - {{ $t('attempt :attempt', status) }}
                 </span>
             </div>
             <div>
-                {{ activity.formatted_duration }}
+                {{ formatted_duration }}
             </div>
         </div>
     </div>
